7fb33b4d2b04a89dd02043e13003a498625fc798
Yauhen Artsiukhou <jsirex@gmail.com>
2021-01-28 14:33:45 +0300

    WIP: Use-package tunning

--- early-init.el
+++ early-init.el
@@ -1,42 +1,37 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
-;;; before most of its package and UI initialization happens.
+;;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el.
+;;; This file is loaded before the package system and GUI is initialized
 
-(setq lexical-binding t)
+(customize-set-value 'package-enable-at-startup nil "We will manually initialize packages")
 
-;; Temporary turn off file name handlers
-(defvar file-name-handler-alist-original file-name-handler-alist)
-(setq file-name-handler-alist nil)
+
 
-;; Defer garbage collection further back in the startup process
-(setq gc-cons-threshold most-positive-fixnum)
+;; During startup we temporary increase gc
+(customize-set-variable 'gc-cons-threshold (* 256 1024 1024))
+(add-hook 'emacs-startup-hook
+          (lambda () (customize-set-variable 'gc-cons-threshold (* 16 1024 1024)) (garbage-collect)))
 
-;; Restore garbage collection and file name handlers
-(add-hook 'after-init-hook
-          (lambda ()
-            (setq gc-cons-threshold 134217728 ; 128 MB
-                  gc-cons-percentage 0.1
-                  file-name-handler-alist file-name-handler-alist-original)
-            (garbage-collect)))
+
 
-;; Faster to disable these here (before they've been initialized)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(horizontal-scroll-bar-mode -1)
-
-;; We're going to manually initialize packages
-(setq package-enable-at-startup nil)
-
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
-(setq frame-inhibit-implied-resize t)
+;; Settings
+(customize-set-variable 'frame-inhibit-implied-resize t "Resize can be a terribly expensive")
+(customize-set-variable 'history-length 100)
+(customize-set-variable 'horizontal-scroll-bar-mode nil)
+(customize-set-variable 'indent-tabs-mode nil "By default do not insert tabs")
+(customize-set-variable 'inhibit-compacting-font-caches t)
+(customize-set-variable 'menu-bar-mode nil)
+(customize-set-variable 'scroll-bar-mode nil)
+(customize-set-variable 'scroll-conservatively 101 "Set modern scrolling behavior")
+(customize-set-variable 'sentence-end-double-space nil)
+(customize-set-variable 'tab-width 4)
+(customize-set-variable 'tool-bar-mode nil)
+(customize-set-variable 'tooltip-mode nil)
 
 ;; Early set font face
-(add-to-list 'default-frame-alist '(font . "FuraCode Nerd Font Mono"))
 (set-face-attribute 'default nil :family "FuraCode Nerd Font Mono" :height 80)
+
+
 
 ;; These so obvious so I put it there
 (defalias 'yes-or-no-p 'y-or-n-p)
